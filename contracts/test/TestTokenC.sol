3d3357ce18812fa5778f356de8c20150e84e4e09
Francesco 'makevoid' Canessa <makevoid@gmail.com>
2025-09-22 08:26:43 +0200

    add Test Token Contracts to test real use cases

---
+++ contracts/test/TestTokenC.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract TestTokenC is ERC20 {
+    constructor() ERC20("Test Token C", "TTC") {
+        _mint(msg.sender, 1000000 * 10**18); // 1M tokens
+    }
+
+    function mint(address to, uint256 amount) public {
+        _mint(to, amount);
+    }
+}
